7ab00243db151a572ecc31f86435358ae166c746
corinachistol <chistolcorina19@gmail.com>
2023-11-26 22:51:29 +0200

    flake & Snowfall initial version

---
+++ src/flake/ui.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './flake.css'
+
+const Component = ({name}) => {
+
+    return (
+        <div className={name}></div>
+    )
+}
+
+//decorator
+const withCoordinates = (Component) => {
+    return ({top,left, speed, ...props}) => {
+        return (
+            <div style={{top:`${top}px`, left:`${left}px`, speed:`${speed}`, position:'relative'}}>
+                <Component {...props} />
+            </div>
+        )
+    }
+}
+const Flake = withCoordinates(Component)
+
+
+const Snowfall = () => {
+
+    // const [flakes,setFlakes] = useState([])
+
+    function randCoord (min,max) {
+        return Math.floor(min + (max-min) * Math.random())
+    }
+    function randFloat (value) {
+        return value * Math.random()
+    }
+
+    return (
+            <Flake top={randCoord(0,window.innerHeight)} 
+                    left={randCoord(0,window.innerWidth)} 
+                    name="flake"
+                    speed={randFloat(10)} />
+    )
+}
+
+export {Snowfall}
+
